856e101e4d17d6832ddf093ea7fc2bb5c17a835f
EmjayFlight <memjay3279@gmail.com>
2018-02-09 06:59:50 -0500

    added column of 1's for preprocessing

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -27,3 +27,5 @@ regressor.fit(X_train, y_train)
 
 y_pred = regressor.predict(X_test)
 
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
