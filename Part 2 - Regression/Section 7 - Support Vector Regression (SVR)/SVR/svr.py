d46aa35ca698fc58c2ba439cc01e19477e068ee2
EmjayFlight <memjay3279@gmail.com>
2018-02-13 22:11:05 -0500

    SVR completed in PY

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/svr.py
@@ -15,6 +15,7 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
+
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
